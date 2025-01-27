366790cb81e4cfa30e2193c0338b3f0a8c1d6867
dannyoga10 <dnnpradipta@gmail.com>
2025-01-27 15:32:23 +0800

    final

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
 function calculateBMI(event) {
-  event.preventDefault();
+  event.preventDefault(); // mencegah form untuk melakukan submit dan merefresh halaman
+
+  // mengambil nilai dari input berat badan dan tinggi badan
   const weight = document.getElementById("input-berat-badan").value;
   const height = document.getElementById("input-tinggi-badan").value;
 
+  //cek apakah nilai berat badan dan tinggi badan valid
   if (weight > 0 && height > 0) {
-    const heightInMeters = height / 100; // Convert height from cm to meters
+    const heightInMeters = height / 100; // mengkonversi tinggi badan dari cm ke m
     const bmi = (weight / (heightInMeters * heightInMeters)).toFixed(2);
-    document.getElementById("result-bmi").innerText = bmi;
+    document.getElementById("result-bmi").innerText = bmi; // Menampilkan hasil perhitungan BMI
 
     let status = "";
     let kategori = "";
+
+    // menentukan status dan kategori BMI
     if (bmi < 18.5) {
       status = "Kekurangan berat badan";
       kategori =
@@ -27,11 +32,30 @@ function calculateBMI(event) {
       kategori =
         "Anda termasuk dalam kategori Kegemukan (overweight), cara terbaik untuk menurunkan berat badan adalah dengan mengurangi asupan kalori dan berolahraga secara rutin. ";
     }
+
+    // Menampilkan status dan kategori BMI
     document.getElementById("result-status").innerText = status;
     document.getElementById("result-description").innerText = kategori;
   } else {
-    alert("Please enter valid weight and height");
+    alert("Please enter valid weight and height"); // Menampilkan pesan error jika nilai berat badan atau tinggi badan tidak valid
   }
 }
 
+// Menjalankan fungsi calculateBMI ketika form di submit
 document.querySelector("form").addEventListener("submit", calculateBMI);
+
+// fungsi untuk mereset form
+function resetForm() {
+  // mengosongkan input field
+  document.getElementById("input-berat-badan").value = "";
+  document.getElementById("input-tinggi-badan").value = "";
+  document.getElementById("input-usia").value = "";
+
+  // mengosongkan hasil perhitungan BMI
+  document.getElementById("result-bmi").innerText = "";
+  document.getElementById("result-status").innerText = "";
+  document.getElementById("result-description").innerText = "";
+}
+
+// Menjalankan fungsi resetForm ketika tombol reset di klik
+document.getElementById("reset-button").addEventListener("click", resetForm);
